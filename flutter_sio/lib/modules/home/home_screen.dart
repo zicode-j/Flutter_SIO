08ec72d3fec1026ff2b8be910496e81e57d45823
zicode-j <83664074+zicode-j@users.noreply.github.com>
2023-07-12 01:16:41 +0700

    feat: OnLoading (Error: gak fungsi)
    Fungsi _OnLoading masih gagal load next fetch
    Note: Data tidak berubah, masih tetap load data yang sama :(

--- flutter_sio/lib/modules/home/home_screen.dart
+++ flutter_sio/lib/modules/home/home_screen.dart
@@ -73,6 +73,52 @@ class _HomeScreenState extends State<HomeScreen> {
   }
 
   void _onLoading() async {
+    skip += 0;
+
+    Map<String, String> queryParameters = {
+      'q': '',
+      'limit': '10',
+      'skip': skip.toString(),
+    };
+
+    try {
+      Uri uri = Uri.https(
+        ApiEndpoint.baseUrl,
+        ApiEndpoint.products,
+        queryParameters,
+      );
+
+      Response response = await http.get(uri);
+
+      if (response.statusCode == 200) {
+        // convert to json
+        final responseJson = json.decode(response.body);
+
+        // convert to model
+        final result = GetProducts.fromJson(responseJson);
+
+        if (result.products.isEmpty) {
+          _refreshController.loadNoData();
+        } else {
+          // update list
+          setState(() {
+            products = result.products;
+          });
+
+          _refreshController.loadComplete();
+        }
+
+        
+      } else {
+        log(response.body);
+
+        _refreshController.refreshFailed();
+      }
+    } catch (e) {
+      log(e.toString(), name: 'ERROR FETCH DATA');
+
+      _refreshController.refreshFailed();
+    }
   }
 
   @override
